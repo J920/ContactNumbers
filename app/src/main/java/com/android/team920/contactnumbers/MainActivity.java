747abcdcf4f9bff735b81eaf1b452b12a61b3c2f
J920 <39294857+J920@users.noreply.github.com>
2019-02-20 15:18:45 +0300

    0.1

--- app/src/main/java/com/android/team920/contactnumbers/MainActivity.java
+++ app/src/main/java/com/android/team920/contactnumbers/MainActivity.java
@@ -9,7 +9,6 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -39,9 +38,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
+            navigation.setSelectedItemId(R.id.navigation_search);
+
+
+
+    }
 }
--- app/src/main/java/com/android/team920/contactnumbers/Search.java
+++ app/src/main/java/com/android/team920/contactnumbers/Search.java
@@ -1,19 +1,28 @@
 package com.android.team920.contactnumbers;
 
 import android.content.Context;
+import android.graphics.drawable.RippleDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.HashSet;
+import java.util.List;
 
 
 public class Search extends Fragment {
 
+    TextView addButton;
+    private List<String> numbersList;
+    RecyclerView recyclerView;
 
     public Search() {
         // Required empty public constructor
@@ -44,6 +53,23 @@ public class Search extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+
+        addButton = view.findViewById(R.id.addButton);
+
+
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//                view.setBackgroundColor(R.drawable.add_on_click_effect);
+            }
+        });
+
+
+        recyclerView = view.findViewById(R.id.num_rec);
+      //  numbersList =new  List();
 
 
     }
